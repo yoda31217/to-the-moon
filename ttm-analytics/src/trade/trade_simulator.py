61b4d6498f308b961dc7c88150634f84a1f6356e
yoda31217 <jbourne.nik@gmail.com>
2023-04-20 19:08:40 +0300

    analytics 2 improved

--- ttm-analytics/src/trade/trade_simulator.py
+++ ttm-analytics/src/trade/trade_simulator.py
@@ -10,10 +10,12 @@ from trade.trade_simulator_tick import TradeSimulatorTick
 class TradeSimulator:
     check_point_tick: TradeSimulatorTick | None
     orders: [TradeSimulatorOrder]
+    closed_orders: [TradeSimulatorOrder]
 
     def __init__(self) -> None:
         self.check_point_tick = None
         self.orders: [TradeSimulatorOrder] = []
+        self.closed_orders = []
 
     def process_ticks(self, ticks: pd.DataFrame, price_step_ratio: float):
         for i in range(len(ticks.timestamp)):
@@ -37,29 +39,27 @@ class TradeSimulator:
                 self.check_point_tick = tick
                 # print(f"New Order: {order.id} {order.open_tick.get_date_time()} {order.type} {order.get_open_price()}")
 
-    def get_closed_orders(self):
-        return list(filter(lambda order: not order.is_open, self.orders))
+            self.closed_orders.extend(list(filter(lambda order: not order.is_open, self.orders)))
+            self.orders = list(filter(lambda order: order.is_open, self.orders))
 
     def get_cumulative_profit(self) -> float:
-        return reduce(lambda profit, order: profit + order.get_profit(), self.get_closed_orders(), 0)
+        return reduce(lambda profit, order: profit + order.get_profit(), self.closed_orders, 0)
 
     def get_profits(self) -> [float]:
-        closed_orders = self.get_closed_orders()
-
         cumulative_profit: float = 0
         cumulative_profits: [float] = []
 
-        for closed_order in closed_orders:
+        for closed_order in self.closed_orders:
             cumulative_profit = cumulative_profit + closed_order.get_profit()
             cumulative_profits.append(cumulative_profit)
 
         return pd.DataFrame({
-            'open_timestamp': list((closed_order.open_tick.timestamp for closed_order in closed_orders)),
-            'type': list((closed_order.type.name for closed_order in closed_orders)),
-            'open_price': list((closed_order.get_open_price() for closed_order in closed_orders)),
-            'close_price': list((closed_order.get_close_price() for closed_order in closed_orders)),
-            'close_timestamp': list((closed_order.close_tick.timestamp for closed_order in closed_orders)),
-            'profit': list((closed_order.get_profit() for closed_order in closed_orders)),
+            'open_timestamp': list((closed_order.open_tick.timestamp for closed_order in self.closed_orders)),
+            'type': list((closed_order.type.name for closed_order in self.closed_orders)),
+            'open_price': list((closed_order.get_open_price() for closed_order in self.closed_orders)),
+            'close_price': list((closed_order.get_close_price() for closed_order in self.closed_orders)),
+            'close_timestamp': list((closed_order.close_tick.timestamp for closed_order in self.closed_orders)),
+            'profit': list((closed_order.get_profit() for closed_order in self.closed_orders)),
             'cumulative_profit': cumulative_profits,
         })
 
