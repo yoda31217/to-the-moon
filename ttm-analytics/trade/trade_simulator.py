c48787cb88ffca090029e1e4ba7d5a9a577f6154
yoda31217 <jbourne.nik@gmail.com>
2023-04-20 12:52:35 +0300

    improving idea intellisense

--- ttm-analytics/trade/trade_simulator.py
+++ ttm-analytics/trade/trade_simulator.py
@@ -1,14 +1,21 @@
 import pandas as pd
+# from binance.binance_k_line_loader import load_binance_k_lines
+# from binance.binance_tick_loader import load_binance_ticks
+# from chart.ttm_chart import draw_line_chart
 
 
 class TradeSimulatorTick:
     def __init__(self, timestamp: int, bid_price: float, ask_price: float):
-        self.timestamp = timestamp
-        self.bid_price = bid_price
-        self.ask_price = ask_price
+        self.timestamp: int = timestamp
+        self.bid_price: float = bid_price
+        self.ask_price: float = ask_price
 
 
 class TradeSimulator:
+
+    def __init__(self) -> None:
+        self.checkPointTick: TradeSimulatorTick | None = None
+
     def process_ticks(self, ticks: pd.DataFrame):
         ticks2 = []
 
@@ -20,3 +27,10 @@ class TradeSimulator:
             ticks2.append(tick)
 
         return ticks2
+
+
+
+# k_lines = load_binance_k_lines(f"../ttm-data/{symbol}-{symbol_k_lines_interval}-2023-03-01.csv")
+#
+# ticks = load_binance_ticks(k_lines, symbol_ask_bid_price_difference)
+
