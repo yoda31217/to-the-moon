c634fcf6ff83a91373fe60424312529460740b9c
yoda31217 <jbourne.nik@gmail.com>
2023-04-20 13:26:05 +0300

    trade simulator improved

--- ttm-analytics/src/trade/trade_simulator.py
+++ ttm-analytics/src/trade/trade_simulator.py
@@ -18,19 +18,35 @@ class TradeSimulatorTick:
     def get_date_time(self):
         return datetime.utcfromtimestamp(self.timestamp / 1000)
 
+    def is_growth_step(self, tick: 'TradeSimulatorTick', price_step_ratio: float):
+        return tick.bid_price >= self.ask_price + self.ask_price * price_step_ratio
+
+    def is_falling_step(self, tick: 'TradeSimulatorTick', price_step_ratio: float):
+        return tick.ask_price <= self.bid_price - self.bid_price * price_step_ratio
+
 
 class TradeSimulator:
 
     def __init__(self) -> None:
         self.check_point_tick: TradeSimulatorTick | None = None
 
-    def process_ticks(self, ticks: pd.DataFrame):
+    def process_ticks(self, ticks: pd.DataFrame, price_step_ratio: float):
         for i in range(len(ticks.timestamp)):
             tick = self._get_tick(ticks, i)
 
             if self.check_point_tick is None:
                 self.check_point_tick = tick
-                print(f"Initial checkpoint set at {tick.get_date_time()}")
+                print(f"Initial checkpoint set at {tick.get_date_time()} and bid price: {tick.bid_price}")
+                continue
+
+            if self.check_point_tick.is_growth_step(tick, price_step_ratio):
+                self.check_point_tick = tick
+                print(f"Growth checkpoint set at {tick.get_date_time()} and bid price: {tick.bid_price}")
+                continue
+
+            if self.check_point_tick.is_falling_step(tick, price_step_ratio):
+                self.check_point_tick = tick
+                print(f"Falling checkpoint set at {tick.get_date_time()} and bid price: {tick.bid_price}")
                 continue
 
     @staticmethod
@@ -45,4 +61,4 @@ k_lines = load_binance_k_lines(f"../../../ttm-data/ETHUSDT-1s-2023-03-01.csv")
 ticks = load_binance_ticks(k_lines, 0.01)
 
 trade_simulator: TradeSimulator = TradeSimulator()
-trade_simulator.process_ticks(ticks)
+trade_simulator.process_ticks(ticks, 0.01)
