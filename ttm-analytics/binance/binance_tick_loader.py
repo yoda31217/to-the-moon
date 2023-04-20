81f74ab7563dc58fc5fb7e59eaa6935404906163
yoda31217 <jbourne.nik@gmail.com>
2023-04-20 12:11:19 +0300

    line graph improved

--- ttm-analytics/binance/binance_tick_loader.py
+++ ttm-analytics/binance/binance_tick_loader.py
@@ -4,7 +4,7 @@ from pandas import DataFrame
 
 def load_binance_ticks(k_lines_data_frame: DataFrame, ask_bid_price_difference: float):
     return pd.DataFrame({
-        'timestamp_millis': k_lines_data_frame.open_timestamp_millis,
+        'timestamp': k_lines_data_frame.open_timestamp_millis,
         'bid_price': k_lines_data_frame.open_price,
         'ask_price': k_lines_data_frame.open_price + ask_bid_price_difference,
     })
--- ttm-analytics/chart/ttm_chart.py
+++ ttm-analytics/chart/ttm_chart.py
@@ -1,15 +1,15 @@
 import datetime as dt
 
-import matplotlib.dates as dates
-import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+sns.set_theme()
 
 
-def draw_line_chart(timestamps: [int], values: [float], color: str = '#4caf50'):
-    date_times = [dt.datetime.utcfromtimestamp(timestampSeconds / 1000) for timestampSeconds in timestamps]
+def draw_line_chart(timestamps: [int], values: [float], name: str):
+    plot = sns.lineplot(data=pd.DataFrame({
+        'Time': (dt.datetime.utcfromtimestamp(timestamp / 1000) for timestamp in timestamps),
+        name: values,
+    }), x='Time', y=name, linewidth=1.0)
 
-    figure = plt.figure()
-    axes = figure.add_subplot()
-    axes.xaxis.set_major_formatter(dates.DateFormatter('%y-%m-%d %H:%M:%S'))
-    plt.xticks(rotation=60)
-    plt.grid(color='#bdbdbd', linestyle='--', linewidth=0.5)
-    axes.plot(date_times, values, color, linewidth=1)
+    plot.set_xticklabels(plot.get_xticklabels(), rotation=60)
