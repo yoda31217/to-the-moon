7732257ff43715b76a9b6623a7cffbdf12aab311
yoda31217 <jbourne.nik@gmail.com>
2023-04-20 04:57:45 +0300

    analytics 2 implemented with charts

--- ttm-analytics/binance/binance_k_line_loader.py
+++ ttm-analytics/binance/binance_k_line_loader.py
@@ -11,7 +11,7 @@ def load_binance_k_lines(csv_files_pattern: str):
 
 
 def _join_binance_k_lines_data_frames(k_lines_data_frames):
-    return pd.concat(k_lines_data_frames).sort_values(by=['openTimestampMillis'])
+    return pd.concat(k_lines_data_frames).sort_values(by=['open_timestamp_millis'])
 
 
 def _load_binance_k_lines_data_frames(csv_file_paths):
@@ -21,6 +21,6 @@ def _load_binance_k_lines_data_frames(csv_file_paths):
 def _load_binance_k_lines_data_frame(csv_file_path: str):
     return pd.read_csv(
         csv_file_path, sep=",", header=None,
-        names=["openTimestampMillis", "openPrice", "highPrice", "lowPrice", "closePrice", "volume",
-               "closeTimestampMillis", "quoteAssetVolume", "tradesCount", "takerBuyBaseAssetVolume",
-               "takerBuyQuoteAssetVolume", "ignore"])
+        names=["open_timestamp_millis", "open_price", "high_price", "low_price", "close_price", "volume",
+               "close_timestamp_millis", "quote_asset_volume", "trades_count", "taker_buy_base_asset_volume",
+               "taker_buy_quote_asset_volume", "ignore"])
