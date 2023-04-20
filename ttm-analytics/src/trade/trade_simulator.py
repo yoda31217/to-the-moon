ad151ff471be4545b51c1f8f91346d8c9a4b5dde
yoda31217 <jbourne.nik@gmail.com>
2023-04-20 14:21:05 +0300

    trade simulator improved

--- ttm-analytics/src/trade/trade_simulator.py
+++ ttm-analytics/src/trade/trade_simulator.py
@@ -39,10 +39,43 @@ class TradeSimulatorOrder:
     type: TradeSimulatorOrderType
     open_tick: TradeSimulatorTick
     close_tick: TradeSimulatorTick | None
+    is_open: bool
+    stop_loss_take_profit_ratio: float
 
-    def __init__(self, tick: TradeSimulatorTick, type: TradeSimulatorOrderType) -> None:
+    def __init__(self, tick: TradeSimulatorTick, type: TradeSimulatorOrderType, stop_loss_take_profit_ratio: float):
         self.type = type
         self.open_tick = tick
+        self.close_tick = None
+        self.is_open = True
+        self.stop_loss_take_profit_ratio = stop_loss_take_profit_ratio
+
+    def get_profit(self):
+        return self._get_profit(self.close_tick)
+
+    def close(self, tick: TradeSimulatorTick):
+        self.close_tick = tick
+        self.is_open = False
+        print(f"Closed Order: {tick.get_date_time()}, {self.type}, "
+              + f"{tick.bid_price if self.type is TradeSimulatorOrderType.BUY else tick.ask_price}")
+
+    def on_new_tick(self, tick: TradeSimulatorTick):
+        if not self.is_open:
+            return
+
+        if self._should_auto_close(tick):
+            self.close(tick)
+
+    def _get_profit(self, new_tick: TradeSimulatorTick):
+        return (new_tick.bid_price - self.open_tick.ask_price
+                if self.type is TradeSimulatorOrderType.BUY
+                else self.open_tick.bid_price - new_tick.ask_price)
+
+    def _should_auto_close(self, tick: TradeSimulatorTick):
+        profit = self._get_profit(tick)
+        profit_ratio = (profit / self.open_tick.ask_price
+                        if self.type is TradeSimulatorOrderType.BUY
+                        else profit / self.open_tick.bid_price)
+        return profit_ratio >= abs(self.stop_loss_take_profit_ratio)
 
 
 class TradeSimulator:
@@ -51,30 +84,29 @@ class TradeSimulator:
 
     def __init__(self) -> None:
         self.check_point_tick = None
-        self.orders = []
+        self.orders: [TradeSimulatorOrder] = []
 
     def process_ticks(self, ticks: pd.DataFrame, price_step_ratio: float):
         for i in range(len(ticks.timestamp)):
             tick = self._get_tick(ticks, i)
 
+            order: TradeSimulatorOrder
+            for order in self.orders:
+                order.on_new_tick(tick)
+
             if self.check_point_tick is None:
                 self.check_point_tick = tick
                 print(f"Initial checkpoint set at {tick.get_date_time()} and bid price: {tick.bid_price}")
-                continue
 
-            if self.check_point_tick.is_growth_step(tick, price_step_ratio):
-                order = TradeSimulatorOrder(tick, TradeSimulatorOrderType.SELL)
-                self.orders.append(order)
+            elif self.check_point_tick.is_growth_step(tick, price_step_ratio):
+                self.orders.append(TradeSimulatorOrder(tick, TradeSimulatorOrderType.SELL, price_step_ratio))
                 self.check_point_tick = tick
                 print(f"New Order: {tick.get_date_time()}, SELL, {tick.bid_price}")
-                continue
 
-            if self.check_point_tick.is_falling_step(tick, price_step_ratio):
-                order = TradeSimulatorOrder(tick, TradeSimulatorOrderType.BUY)
-                self.orders.append(order)
+            elif self.check_point_tick.is_falling_step(tick, price_step_ratio):
+                self.orders.append(TradeSimulatorOrder(tick, TradeSimulatorOrderType.BUY, price_step_ratio))
                 self.check_point_tick = tick
                 print(f"New Order: {tick.get_date_time()}, BUY , {tick.ask_price}")
-                continue
 
     @staticmethod
     def _get_tick(ticks, i):
