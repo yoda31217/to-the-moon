133cdc5f3d9fc942dbd1b1fef1d0f48aaed8424f
yoda31217 <jbourne.nik@gmail.com>
2023-04-21 21:38:19 +0300

    transactions class improved

--- ttm-analytics/src/trade/trade_simulator_transactions.py
+++ ttm-analytics/src/trade/trade_simulator_transactions.py
@@ -7,3 +7,15 @@ class TradeSimulatorTransactions:
     def __init__(self, data_frame: pd.DataFrame) -> None:
         super().__init__()
         self.data_frame = data_frame
+
+    def get_count(self):
+        return len(self.data_frame.index)
+
+    def get_average_price_margin(self):
+        return self.data_frame.price_margin.mean()
+
+    def get_average_profit(self):
+        return self.data_frame.profit.mean()
+
+    def get_cumulative_profit(self):
+        return self.data_frame.cumulative_profit.iloc[-1]
