39fec6e99dea483c0d012541b5e20e374cc6de81
yoda31217 <jbourne.nik@gmail.com>
2023-04-21 13:32:47 +0300

    simulator re-usability improved

--- ttm-analytics/src/chart/ttm_chart.py
+++ ttm-analytics/src/chart/ttm_chart.py
@@ -1,4 +1,4 @@
-import datetime as dt
+from datetime import datetime
 
 import pandas as pd
 import seaborn as sns
@@ -8,19 +8,28 @@ sns.set_theme()
 
 
 def draw_line_chart(data_frame: pd.DataFrame, timestamp_column_name: str, value_column_name: str,
-    value_label: str, title: str):
+                    value_label: str, title: str):
     sampling_limit = 100000
     sampled_data_frame: pd.DataFrame = (data_frame.iloc[::round(data_frame.size / sampling_limit), :]
                                         if data_frame.size > sampling_limit else
                                         data_frame)
 
     formatted_data_frame = pd.DataFrame({
-        'Date Time': (dt.datetime.utcfromtimestamp(timestamp / 1000)
-                      for timestamp in sampled_data_frame[timestamp_column_name]),
+        'Date Time': sampled_data_frame[timestamp_column_name],
         value_label: sampled_data_frame[value_column_name],
     })
 
     fig, ax = plt.subplots(figsize=(16, 9))
     plot = sns.lineplot(data=formatted_data_frame, x='Date Time', y=value_label, linewidth=1.0, ax=ax)
+
+    min_timestamp = sampled_data_frame[timestamp_column_name].min()
+    max_timestamp = sampled_data_frame[timestamp_column_name].max()
+    plot_ticks_count = 40
+    plot_x_ticks = [min_timestamp + round(i * (max_timestamp - min_timestamp) / float(plot_ticks_count))
+                    for i in range(0, plot_ticks_count + 1)]
+    plot.set_xticks(plot_x_ticks)
+    plot.set_xticklabels([datetime.utcfromtimestamp(plot_x_tick / 1000).strftime("%m/%d %H:%M")
+                          for plot_x_tick in plot_x_ticks])
+
     plot.set(title=f"{title} (sampled)")
-    plot.set_xticklabels(plot.get_xticklabels(), rotation=60)
+    plot.set_xticklabels(plot.get_xticklabels(), rotation=90)
