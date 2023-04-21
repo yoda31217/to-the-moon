8a0d7fd6fec494b0b864213e6d851e69f41b1a39
yoda31217 <jbourne.nik@gmail.com>
2023-04-21 13:40:38 +0300

    simulator charts improved

--- ttm-analytics/src/chart/ttm_chart.py
+++ ttm-analytics/src/chart/ttm_chart.py
@@ -24,12 +24,20 @@ def draw_line_chart(data_frame: pd.DataFrame, timestamp_column_name: str, value_
 
     min_timestamp = sampled_data_frame[timestamp_column_name].min()
     max_timestamp = sampled_data_frame[timestamp_column_name].max()
-    plot_ticks_count = 40
-    plot_x_ticks = [min_timestamp + round(i * (max_timestamp - min_timestamp) / float(plot_ticks_count))
-                    for i in range(0, plot_ticks_count + 1)]
+    plot_x_ticks_count = 40
+    plot_x_ticks = [min_timestamp + round(i * (max_timestamp - min_timestamp) / float(plot_x_ticks_count))
+                    for i in range(0, plot_x_ticks_count + 1)]
     plot.set_xticks(plot_x_ticks)
     plot.set_xticklabels([datetime.utcfromtimestamp(plot_x_tick / 1000).strftime("%m/%d %H:%M")
                           for plot_x_tick in plot_x_ticks])
+
+    min_value = sampled_data_frame[value_column_name].min()
+    max_value = sampled_data_frame[value_column_name].max()
+    plot_y_ticks_count = 20
+    plot_y_ticks = [min_value + round(i * (max_value - min_value) / float(plot_y_ticks_count))
+                    for i in range(0, plot_y_ticks_count + 1)]
+    plot.set_yticks(plot_y_ticks)
+    plot.set_yticklabels([f"{plot_y_tick:,.2f}" for plot_y_tick in plot_y_ticks])
 
     plot.set(title=f"{title} (sampled)")
     plot.set_xticklabels(plot.get_xticklabels(), rotation=90)
