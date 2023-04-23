8b08dd38305fea88b1f4c92c36eb6f646e072451
yoda31217 <jbourne.nik@gmail.com>
2023-04-23 11:49:36 +0300

    report summary added

--- simulator/streamlit.py
+++ simulator/streamlit.py
@@ -1,16 +1,21 @@
 import streamlit as st
+import pandas as pd
 from binance.binance_k_line_loader import load_binance_k_lines
 from binance.binance_tick_loader import load_binance_ticks
 from bot.bot_0_strategy import Bot0Strategy
 from chart.chart import draw_line_chart
 from trade.trade_simulator import TradeSimulator
 
+# Options
+
 st.sidebar.header("Опции")
 
 st.sidebar.subheader("Стратегия: Bot0")
 
 symbol = "ETHUSDT"
+
 symbol_ask_bid_price_difference = 0.01
+
 price_step_ratio = (
     st.sidebar.slider(
         "Шаг цены, %",
@@ -22,12 +27,15 @@ price_step_ratio = (
     )
     / 100.0
 )
+
 inverted = st.sidebar.checkbox(
     "Инвертировать", value=False, help="Поменять местами покупку и продажу"
 )
 
+strategy = Bot0Strategy(price_step_ratio, inverted)
 
-st.title("Результаты симуляции")
+# Body
+
 
 st.text("Description1")
 
@@ -39,34 +47,50 @@ ticks = load_binance_ticks(k_lines, symbol_ask_bid_price_difference)
 
 st.subheader(f"Цена покупки, symbol={symbol}")
 
-draw_line_chart(ticks, "timestamp", "bid_price", "Цена покупки ($)")
+draw_line_chart(ticks, "timestamp", "bid_price", "Цена покупки, $")
 
 trade_simulator: TradeSimulator = TradeSimulator(ticks)
 
-st.subheader(f"Profit")
+st.header("Результаты симуляции")
 
-#     # 'orders=30,049 interval_days=48.0 avg_tick_price_change=0.06 str=Bot0[0.10%, not_inverted]
-#     # tx_avg_price_margin=1.99 tx_avg_prof=-0.04 tx_cum_prof=-1199.56'
-
-result = trade_simulator.simulate(Bot0Strategy(price_step_ratio, inverted))
+result = trade_simulator.simulate(strategy)
 
 if result.get_transactions_count() > 0:
     draw_line_chart(
         result.transactions,
         "open_timestamp",
         "cumulative_profit",
-        "Итоговая прибыль ($)",
+        "Итоговая прибыль, $",
     )
 else:
     st.caption("No Transactions! 😕")
 
-    # result_str = (f"orders={result.get_transactions_count() :,}"
-    #               f" interval_days={result.get_interval_days():.1f}"
-    #               f" avg_tick_price_change={result.get_average_ticks_price_change():.2f}"
-    #               f" str={strategy}"
-    #               f" tx_avg_price_margin={result.get_transactions_average_price_margin():.2f}"
-    #               f" tx_avg_prof={result.get_transactions_average_profit():.2f}"
-    #               f" tx_cum_prof={result.get_transactions_cumulative_profit():.2f}")
+st.subheader(f"Сводка")
 
-    # display(result_str)
-    # display(result)
+# 'orders=30,049 interval_days=48.0 avg_tick_price_change=0.06 str=Bot0[0.10%, not_inverted]
+# tx_avg_price_margin=1.99 tx_avg_prof=-0.04 tx_cum_prof=-1199.56'
+
+st.table(
+    pd.DataFrame(
+        {
+            "Показатель": [
+                "Количество транзакций",
+                "Интервал симуляции, день",
+                "Среднее изменение цены за тик",
+                "Название стратегии",
+                "Средняя ценовая маржа транзакции",
+                "Средняя прибыль транзакции",
+                "Итоговая прибыль",
+            ],
+            "Значение": [
+                result.get_transactions_count(),
+                "{:.1f}".format(result.get_interval_days()),
+                "{:.2f}".format(result.get_average_ticks_price_change()),
+                strategy,
+                "{:.2f}".format(result.get_transactions_average_price_margin()),
+                "{:.2f}".format(result.get_transactions_average_profit()),
+                "{:.2f}".format(result.get_transactions_cumulative_profit()),
+            ],
+        }
+    )
+)
