476a850be72a99991ea886eb24136732e37d2e0c
yoda31217 <jbourne.nik@gmail.com>
2023-04-24 15:35:47 +0300

    ui refactoring

--- simulator/main.py
+++ simulator/main.py
@@ -5,7 +5,18 @@ from binance.binance_k_line_loader import BINANCE_SYMBOLS, load_binance_k_lines
 from binance.binance_tick_loader import load_binance_ticks
 
 from bot.bot_0_strategy import Bot0Strategy
-from report.report import report_profit_chart, report_summary, report_ticks_chart
+from report.report import (
+    input_date_from_widget,
+    input_date_to_widget,
+    input_inverted_widget,
+    input_price_step_ratio_widget,
+    input_symbol_ask_bid_price_difference_widget,
+    input_symbol_widget,
+    report_profit_chart,
+    report_summary,
+    report_ticks_chart,
+    text_description,
+)
 from trade.trade_simulator import TradeSimulator
 
 st.set_page_config(
@@ -14,69 +25,19 @@ st.set_page_config(
 )
 
 st.header(f"Симулятор торговли крипто бота")
-
-# with st.expander("Описание", expanded=False):
-#     st.write(
-#         """
-#             Привет!
-
-#             Это бесплатный и открытый симулятор торговли Бота на крипто бирже Binance. 
-
-#             Мы используем реальные данные и соболюдаем все условия торговли. 
-#             Все сделано для максимально легкого и оперативного тестирования гипотез.
-
-#             Настраивай симуляцию в панели слева и ожидай результат.
-
-#             Проект открытый. Вот
-#             [исходники на GitHub](https://github.com/yoda31217/to-the-moon)
-#             проекта и пример 
-#             [стратегии Бота](https://github.com/yoda31217/to-the-moon/blob/main/simulator/bot/bot_0_strategy.py).
-#         """
-#     )
+text_description()
 
 st.sidebar.title("Опции")
-
 st.sidebar.header("Рынок")
-
 st.sidebar.markdown("**Биржа: Binance**")
-
-symbol: str = str(
-    st.sidebar.selectbox(
-        "Символ", options=BINANCE_SYMBOLS, index=BINANCE_SYMBOLS.index("ETHUSDT")
-    )
-)
-
-date_from: date = cast(
-    date, st.sidebar.date_input("Дата с", date.today() - timedelta(days=2))
-)
-
-date_to: date = cast(
-    date, st.sidebar.date_input("Дата по", date.today() - timedelta(days=2))
-)
-
-symbol_ask_bid_price_difference = st.sidebar.number_input(
-    "Разница цены купли-проажи", value=0.01
-)
-
+symbol: str = input_symbol_widget()
+date_from: date = input_date_from_widget()
+date_to: date = input_date_to_widget()
+symbol_ask_bid_price_difference = input_symbol_ask_bid_price_difference_widget()
 st.sidebar.header("Bot")
-
 st.sidebar.markdown("**Название: Bot0**")
-
-price_step_ratio = (
-    st.sidebar.slider(
-        "Шаг цены, %",
-        min_value=0.1,
-        max_value=10.0,
-        value=1.0,
-        step=0.1,
-        help="На сколько должна измениться цена, что бы бот сделал действие",
-    )
-    / 100.0
-)
-
-inverted = st.sidebar.checkbox(
-    "Инвертировать", value=False, help="Поменять местами покупку и продажу в алгоритме"
-)
+price_step_ratio = input_price_step_ratio_widget()
+inverted = input_inverted_widget()
 
 try:
     strategy = Bot0Strategy(price_step_ratio, inverted)
--- simulator/report/report.py
+++ simulator/report/report.py
@@ -1,10 +1,71 @@
-from datetime import date
+from datetime import date, timedelta
+from typing import cast
 import pandas as pd
 import streamlit as st
+from binance.binance_k_line_loader import BINANCE_SYMBOLS
 from chart.chart import draw_line_chart
 from trade.trade_simulator_result import TradeSimulatorResult
 
 from trade.trade_simulator_strategy import TradeSimulatorStrategy
+
+
+def text_description():
+    with st.expander("Описание", expanded=False):
+        st.write(
+            """
+                Привет!
+                
+                Проект открытый. Вот
+                исходники проекта на [GitHub](https://github.com/yoda31217/to-the-moon)
+                и пример 
+                [Бота](https://github.com/yoda31217/to-the-moon/blob/main/simulator/bot/bot_0_strategy.py).
+            """
+        )
+
+
+def input_symbol_widget():
+    return cast(
+        str,
+        st.sidebar.selectbox(
+            "Символ", options=BINANCE_SYMBOLS, index=BINANCE_SYMBOLS.index("ETHUSDT")
+        ),
+    )
+
+
+def input_date_from_widget():
+    return cast(date, st.sidebar.date_input("Дата с", date.today() - timedelta(days=2)))
+
+
+def input_date_to_widget():
+    return cast(
+        date, st.sidebar.date_input("Дата по", date.today() - timedelta(days=2))
+    )
+
+
+def input_price_step_ratio_widget():
+    return (
+        st.sidebar.slider(
+            "Шаг цены, %",
+            min_value=0.1,
+            max_value=10.0,
+            value=1.0,
+            step=0.1,
+            help="На сколько должна измениться цена, что бы бот сделал действие",
+        )
+        / 100.0
+    )
+
+
+def input_inverted_widget():
+    return st.sidebar.checkbox(
+        "Инвертировать",
+        value=False,
+        help="Поменять местами покупку и продажу в алгоритме",
+    )
+
+
+def input_symbol_ask_bid_price_difference_widget():
+    return st.sidebar.number_input("Разница цены купли-проажи", value=0.01)
 
 
 def report_summary(
@@ -14,9 +75,9 @@ def report_summary(
     strategy: TradeSimulatorStrategy,
     result: TradeSimulatorResult,
 ):
-# values to check (like a test)
-# 'orders=30,049 interval_days=48.0 avg_tick_price_change=0.06 str=Bot0[0.10%, not_inverted]
-# tx_avg_price_margin=1.99 tx_avg_prof=-0.04 tx_cum_prof=-1199.56'
+    # values to check (like a test)
+    # 'orders=30,049 interval_days=48.0 avg_tick_price_change=0.06 str=Bot0[0.10%, not_inverted]
+    # tx_avg_price_margin=1.99 tx_avg_prof=-0.04 tx_cum_prof=-1199.56'
 
     st.subheader(f"Сводка")
     st.table(
