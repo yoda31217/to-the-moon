be3f4a1df8ce4b7ca760e7f1567adaf505114526
yoda31217 <jbourne.nik@gmail.com>
2023-04-25 09:44:39 +0300

    typecheck fixes

--- simulator/trade/trade_simulator.py
+++ simulator/trade/trade_simulator.py
@@ -7,22 +7,26 @@ from trade.trade_simulator_result import TradeSimulatorResult
 from trade.trade_simulator_strategy import TradeSimulatorStrategy
 from trade.trade_simulator_tick import TradeSimulatorTick
 
-TickDataFrameRowTuple = typing.NamedTuple('Employee', timestamp=int, bid_price=float, ask_price=float)
+TickDataFrameRowTuple = typing.NamedTuple(
+    "Employee", timestamp=int, bid_price=float, ask_price=float
+)
 
 
 class TradeSimulator:
-    ticks: [TradeSimulatorTick]
+    ticks: list[TradeSimulatorTick]
     ticks_data_frame: pd.DataFrame
 
     def __init__(self, ticks_data_frame: pd.DataFrame) -> None:
         # self.ticks = list((self._to_tick(tick_row) for tick_index, tick_row in ticks_data_frame.iterrows()))
         tick_row: TickDataFrameRowTuple
-        self.ticks = list((self._to_tick(tick_row) for tick_row in ticks_data_frame.itertuples()))
+        self.ticks = list(
+            (self._to_tick(tick_row) for tick_row in ticks_data_frame.itertuples())
+        )
         self.ticks_data_frame = ticks_data_frame
 
     def simulate(self, strategy: TradeSimulatorStrategy) -> TradeSimulatorResult:
-        orders: [TradeSimulatorOrder] = []
-        closed_orders: [TradeSimulatorOrder] = []
+        orders: list[TradeSimulatorOrder] = []
+        closed_orders: list[TradeSimulatorOrder] = []
 
         for new_tick in self.ticks:
             self._notify_orders(new_tick, orders)
@@ -33,7 +37,11 @@ class TradeSimulator:
 
         return TradeSimulatorResult(closed_orders, self.ticks_data_frame)
 
-    def _close_orders(self, orders: [TradeSimulatorOrder], closed_orders: [TradeSimulatorOrder]):
+    def _close_orders(
+        self,
+        orders: list[TradeSimulatorOrder],
+        closed_orders: list[TradeSimulatorOrder],
+    ):
         order: TradeSimulatorOrder
         for order in orders:
             if order.is_open:
@@ -41,7 +49,9 @@ class TradeSimulator:
         self._move_orders_to_closed(orders, closed_orders)
 
     @staticmethod
-    def _move_orders_to_closed(orders: [TradeSimulatorOrder], closed_orders: [TradeSimulatorOrder]):
+    def _move_orders_to_closed(
+        orders: list[TradeSimulatorOrder], closed_orders: list[TradeSimulatorOrder]
+    ):
         new_closed_orders = [order for order in orders if not order.is_open]
         closed_orders.extend(new_closed_orders)
 
@@ -50,7 +60,7 @@ class TradeSimulator:
         orders.extend(open_orders)
 
     @staticmethod
-    def _notify_orders(tick, orders: [TradeSimulatorOrder]):
+    def _notify_orders(tick, orders: list[TradeSimulatorOrder]):
         order: TradeSimulatorOrder
         for order in orders:
             order.notify(tick)
@@ -58,6 +68,5 @@ class TradeSimulator:
     @staticmethod
     def _to_tick(tick_row: TickDataFrameRowTuple):
         return TradeSimulatorTick(
-            tick_row.timestamp,
-            tick_row.bid_price,
-            tick_row.ask_price)
+            tick_row.timestamp, tick_row.bid_price, tick_row.ask_price
+        )
