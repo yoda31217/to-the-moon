ff6b7bda60b792b668a3f2223f5022ae1b9f11d9
yoda31217 <jbourne.nik@gmail.com>
2023-04-25 10:31:16 +0300

    data added

--- simulator/binance/binance_tick_loader.py
+++ simulator/binance/binance_tick_loader.py
@@ -1,10 +1,13 @@
 import pandas as pd
-from pandas import DataFrame
+from pandas import DataFrame, Series
 
 
 def load_binance_ticks(k_lines_data_frame: DataFrame, ask_bid_price_difference: float):
-    return pd.DataFrame({
-        'timestamp': k_lines_data_frame.open_timestamp_millis,
-        'bid_price': k_lines_data_frame.open_price,
-        'ask_price': k_lines_data_frame.open_price + ask_bid_price_difference,
-    })
+    new_var: Series[int] = k_lines_data_frame.open_timestamp_millis
+    return pd.DataFrame(
+        {
+            "timestamp": new_var,
+            "bid_price": k_lines_data_frame.open_price,
+            "ask_price": k_lines_data_frame.open_price + ask_bid_price_difference,
+        }
+    )
