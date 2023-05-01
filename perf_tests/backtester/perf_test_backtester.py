6766cb0c8849bead4191a823a16771e24a0a82a0
yoda31217 <jbourne.nik@gmail.com>
2023-05-01 21:15:53 +0300

    Draw chart with spendings (not closed orders) in timeline #5

--- perf_tests/backtester/perf_test_backtester.py
+++ perf_tests/backtester/perf_test_backtester.py
@@ -3,17 +3,40 @@ from datetime import date
 from backtester.backtester import Backtester
 from binance import binance_ticker_repository
 from bot.bot_one_step_order import BotOneStepOrder
-import cProfile, pstats, io
+import cProfile, pstats
 from pstats import SortKey
 
+# Log:
+# Optimisation 1:
+# Baseline:
+# ----
+#          34854062 function calls (34854032 primitive calls) in 9.401 seconds
+#
+#    Ordered by: cumulative time
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.008    0.008    9.401    9.401 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/backtester/backtester.py:29(test)
+#      7201    0.005    0.000    6.793    0.001 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/backtester/backtester.py:60(_calculate_and_add_balance)
+#     21603    0.282    0.000    6.782    0.000 {built-in method builtins.sum}
+#      7201    0.005    0.000    5.941    0.001 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/backtester/backtester.py:76(_calculate_margin_balance)
+#   5308698    2.742    0.000    4.971    0.000 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/order/order.py:54(get_pnl)
+#   3278517    1.100    0.000    4.362    0.000 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/backtester/backtester.py:80(<genexpr>)
+#      7200    0.159    0.000    2.134    0.000 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/backtester/backtester.py:100(_notify_orders)
+#   1016585    0.325    0.000    1.976    0.000 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/order/order.py:74(notify)
+#   1016585    0.397    0.000    1.542    0.000 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/order/order.py:107(_should_auto_close)
+#   7347417    1.528    0.000    1.528    0.000 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/order/order.py:81(get_entry_price)
+#   1023987    0.349    0.000    1.350    0.000 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/backtester/backtester.py:77(<genexpr>)
+#   5311372    1.104    0.000    1.104    0.000 /Users/nikita/Documents/projects/yoda31217/to-the-moon/simulator/order/order.py:97(_get_exit_price)
+# ====
+# Final:
 
 symbol = "ETHUSDT"
 date_from = date.fromisoformat("2023-04-23")
-date_to = date.fromisoformat("2023-04-29")
+date_to = date.fromisoformat("2023-04-27")
 bid_ask_spread = 0.01
-step_to_price_ratio = 0.01
+step_to_price_ratio = 0.001
 tp_to_entry_price_ratio = 0.01
-sl_to_entry_price_ratio = -0.01
+sl_to_entry_price_ratio = -0.1
 inverted = False
 positions_sort_timestamp_column = "entry_timestamp"
 
@@ -32,7 +55,5 @@ profiler.enable()
 backtester.test(bot, positions_sort_timestamp_column)
 
 profiler.disable()
-s = io.StringIO()
-ps = pstats.Stats(profiler, stream=s).sort_stats(SortKey.CUMULATIVE)
+ps = pstats.Stats(profiler).sort_stats(SortKey.CUMULATIVE)
 ps.print_stats()
-print(s.getvalue())
