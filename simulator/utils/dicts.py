fc65b1fb513d7f8cd2d3c240e533bd672275ec84
yoda31217 <jbourne.nik@gmail.com>
2023-05-11 11:20:32 +0300

    Extract Bot params and Bot information tab #45

--- simulator/utils/dicts.py
+++ simulator/utils/dicts.py
@@ -1,4 +1,8 @@
 def to_json(dictionary: dict):
     key_value_strs = [f'"{key}": {value}' for (key, value) in dictionary.items()]
-    key_values_str = "\n    ".join(key_value_strs)
-    return f"{{\n    {key_values_str}\n}}"
+    key_values_str = "\n                ".join(key_value_strs)
+    return f"""
+            {{
+                {key_values_str}
+            }}
+            """
