82a411e5bed10c2fde314d39dfab4983e61490fa
yoda31217 <jbourne.nik@gmail.com>
2023-05-12 10:18:04 +0300

    Perf: add caching to backtesting report #37

--- simulator/utils/dicts.py
+++ simulator/utils/dicts.py
@@ -1,8 +1,4 @@
 def to_json(dictionary: dict):
     key_value_strs = [f'"{key}": {value}' for (key, value) in dictionary.items()]
-    key_values_str = "\n                ".join(key_value_strs)
-    return f"""
-            {{
-                {key_values_str}
-            }}
-            """
+    key_values_str = "\n    ".join(key_value_strs)
+    return f"{{\n    {key_values_str}\n}}"
