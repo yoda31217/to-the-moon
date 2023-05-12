a968276646387f644eec77d972d317d7205447e8
yoda31217 <jbourne.nik@gmail.com>
2023-05-12 11:40:46 +0300

    Throw error on close already closed order #39

--- simulator/utils/gits.py
+++ simulator/utils/gits.py
@@ -1,9 +1,22 @@
+import datetime
 from git.repo import Repo
 
 
-def get_version():
-    repo = Repo(search_parent_directories=True)
+def get_version() -> str:
+    repository = get_repository()
     return (
-        f"hash: 0x{repo.head.commit.hexsha[0:8]}..."
-        + f" from {repo.head.commit.committed_datetime}"
+        f"hash: 0x{get_head_commit_hash(repository)[0:8]}..."
+        + f" from {get_head_commit_datetime(repository)}"
     )
+
+
+def get_head_commit_datetime(repository: Repo) -> datetime.datetime:
+    return repository.head.commit.committed_datetime
+
+
+def get_head_commit_hash(repository: Repo) -> str:
+    return repository.head.commit.hexsha
+
+
+def get_repository() -> Repo:
+    return Repo(search_parent_directories=True)
