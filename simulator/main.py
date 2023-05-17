1e3cdabc7605e2772cc6878ae69b0d7682f42d3b
yoda31217 <jbourne.nik@gmail.com>
2023-05-17 16:03:22 +0300

    Implement Grid Bot #2

--- simulator/main.py
+++ simulator/main.py
@@ -7,6 +7,7 @@ from report.report import build_report
 try:
     build_report(
         {
+            # Add any number of new bot(s) here.
             BotOneStepOrder: lambda: {
                 "bot_constructor": BotOneStepOrder,
                 "bot_config": bot_one_step_order_input.config(),
